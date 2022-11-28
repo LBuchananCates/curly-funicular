9046013736b6c0abb2533561aaf79956fbd51d87
Leighton Buchanan-Cates <100169368+LBuchananCates@users.noreply.github.com>
2022-11-28 12:20:25 -0500

    Add files via upload
    formats and styles html containers

--- rating-component.js
+++ rating-component.js
@@ -1,1 +1,7 @@
 console.log("hello world!");
+// 1. when hovering, bg turns light gray and font white, when selected onclick, bg turns orange and font white JS
+// 2. conatiner-two display none but display flex when onclick submit
+//
+//
+//
+//
